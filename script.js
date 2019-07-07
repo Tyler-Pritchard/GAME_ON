d7901253a457f7e4a0b847054b82c6a5993dcd9c
Tyler Pritchard <pritchard.tyler@gmail.com>
2019-07-07 08:42:26 -0700

    update

--- script.js
+++ script.js
@@ -2,3 +2,17 @@ var ref = firebase.database().ref();
 ref.on("value", function (snapshot) {
     output.innerHTML = JSON.stringify(snapshot.val(), null, 2);
 });
+
+//globals: $, jQuery and Tether, see config
+
+import 'bootstrap';
+// adds all custom Bootstrap jQuery plugins
+// see all plugins here: http://getbootstrap.com/javascript/
+
+document.addEventListener('DOMContentLoaded', () => {
+  // do your setup here
+  console.log('Initialized app');
+  if ($('btn')) {
+    console.log('jQuery works')
+  }
+});
